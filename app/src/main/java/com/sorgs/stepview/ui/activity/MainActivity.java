3b302cd5acc4fb0109955b330190e947641e01a6
Sorgs <1042746391@qq.com>
2018-08-17 16:46:33 +0800

    add res

--- app/src/main/java/com/sorgs/stepview/ui/activity/MainActivity.java
+++ app/src/main/java/com/sorgs/stepview/ui/activity/MainActivity.java
@@ -6,9 +6,9 @@ import com.sorgs.stepview.R;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
